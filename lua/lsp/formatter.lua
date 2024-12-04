e6ec18331e3476dfa083196645647829ca675056
Prtysil <1377374583@qq.com>
2024-12-04 19:59:47 +0800

    Add formatter.nivm plugin and finish '*.c', '*.cpp', '*.lua', '*.verilog' format

---
+++ lua/lsp/formatter.lua
@@ -0,0 +1,52 @@
+return {
+	"mhartington/formatter.nvim",
+	config = function()
+		-- Utilities for creating configurations
+		local util = require("formatter.util")
+		-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+		require("formatter").setup({ -- Enable or disable logging
+			logging = true,
+			-- Set the log level
+			log_level = vim.log.levels.WARN,
+			-- All formatter configurations are opt-in
+			filetype = {
+				-- Formatter configurations for filetype "lua" go here
+				-- and will be executed in order
+				lua = {
+					-- "formatter.filetypes.lua" defines default configurations for the
+					-- "lua" filetype
+					require("formatter.filetypes.lua").stylua,
+				},
+				c = {
+					require("formatter.filetypes.c").clangformat,
+				},
+				cpp = {
+					require("formatter.filetypes.cpp").clangformat,
+				},
+				verilog = {
+					function()
+						return {
+							exe = "verible-verilog-format",
+							args = { "--indentation_spaces 4", "-" },
+							stdin = true,
+						}
+					end,
+				},
+
+				-- Use the special "*" filetype for defining formatter configurations on
+				-- any filetype
+				["*"] = {
+					-- "formatter.filetypes.any" defines default configurations for any
+					-- filetype
+					require("formatter.filetypes.any").remove_trailing_whitespace,
+					-- Remove trailing whitespace without 'sed'
+					-- require("formatter.filetypes.any").substitute_trailing_whitespace,
+				},
+			},
+		})
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			pattern = { "*.h", "*.hpp", "*.c", "*.cpp", "*.py", "*.lua", "*.v", "CMakeLists.txt" },
+			command = "FormatWrite",
+		})
+	end,
+}
--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -1,38 +1,41 @@
 return {
-        {
-                "williamboman/mason.nvim",
-                config = function()
-                        require("mason").setup({
-                                ui = {
-                                        icons = {
-                                                package_installed = "✓",
-                                                package_pending = "➜",
-                                                package_uninstalled = "✗"
-                                        }
-                                }
-                        })
-                end,
-        },
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
 
-        {
-                "williamboman/mason-lspconfig.nvim",
-                config = function()
-                        require("mason-lspconfig").setup({
-                                -- 确保安装，根据需要填写
-                                ensure_installed = { "lua_ls", "c", "cpp", "verilog", },
-                        })
-                end,
-        },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- 确保安装，根据需要填写
+				ensure_installed = { "lua_ls", "c", "cpp", "verilog", "systemverilog" },
+			})
+		end,
+	},
 
-
-        {
-                "neovim/nvim-lspconfig",
-                config = function()
-                        -- Set up lspconfig.
-                        local capabilities = require('cmp_nvim_lsp').default_capabilities() -- More details see https://github.com/hrsh7th/nvim-cmp
-                        require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-                        require("lspconfig").clangd.setup({ capabilities = capabilities })
-                        require("lspconfig").verible.setup({ capabilities = capabilities })
-                end,
-        },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Set up lspconfig.
+			local capabilities = require("cmp_nvim_lsp").default_capabilities() -- More details see https://github.com/hrsh7th/nvim-cmp
+			require("lspconfig").lua_ls.setup({ capabilities = capabilities })
+			require("lspconfig").clangd.setup({ capabilities = capabilities })
+			require("lspconfig").verible.setup({
+				capabilities = capabilities,
+				cmd = { "verible-verilog-ls", "--rules_config_search" },
+			})
+		end,
+	},
 }
