1cc346925e09a5d53c31f7748fbdcc0c5b913120
Prtysil <1377374583@qq.com>
2024-12-04 22:56:17 +0800

    Add more plugins

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,18 +1,46 @@
 return {
-  "folke/persistence.nvim",
-  event = "BufReadPre", -- this will only start session saving when an actual file was opened
-  opts = {
-    -- add any custom options here
-  },
+	"folke/persistence.nvim",
+	event = "BufReadPre", -- this will only start session saving when an actual file was opened
+	opts = {
+		-- add any custom options here
+	},
 
-  keys = {
-        -- load the session for the current directory
-        { mode = "n", "<leader>qs", function() require("persistence").load() end, desc = "load"},
-        -- select a session to load
-        { mode = "n", "<leader>qS", function() require("persistence").select() end, desc = "select"},
-        -- load the last session
-        { mode = "n", "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "load" },
-        -- stop Persistence => session won't be saved on exit
-        { mode = "n", "<leader>qd", function() require("persistence").stop() end, desc = "stop" },
-  },
+	keys = {
+		-- load the session for the current directory
+		{
+			mode = "n",
+			"<leader>qs",
+			function()
+				require("persistence").load()
+			end,
+			desc = "load",
+		},
+		-- select a session to load
+		{
+			mode = "n",
+			"<leader>qS",
+			function()
+				require("persistence").select()
+			end,
+			desc = "select",
+		},
+		-- load the last session
+		{
+			mode = "n",
+			"<leader>ql",
+			function()
+				require("persistence").load({ last = true })
+			end,
+			desc = "load last session",
+		},
+		-- stop Persistence => session won't be saved on exit
+		{
+			mode = "n",
+			"<leader>qd",
+			function()
+				require("persistence").stop()
+			end,
+			desc = "stop",
+		},
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,39 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = { "p00f/nvim-ts-rainbow" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- Add which language you want
-			ensure_installed = {
-				"c",
-				"cpp",
-				"lua",
-				"vim",
-				"markdown",
-				"markdown_inline",
-				"verilog",
-				-- "systemverilog",
-				"python",
-				"json",
-				"javascript",
-			},
-			highlight = {
-				enable = true,
-				disable = {},
-			},
-			rainbow = {
-				enable = true,
-				-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-				extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-				max_file_lines = nil, -- Do not enable for files with more than n lines, int
-				-- colors = {}, -- table of hex strings
-				-- termcolors = {} -- table of colour name strings
-			},
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"p00f/nvim-ts-rainbow", -- colored rainbow
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- Add which language you want
+				ensure_installed = {
+					"c",
+					"cpp",
+					"lua",
+					"vim",
+					"markdown",
+					"markdown_inline",
+					"verilog",
+					-- "systemverilog",
+					"python",
+					"json",
+					"javascript",
+				},
+				highlight = {
+					enable = true,
+					disable = {},
+				},
+				rainbow = {
+					enable = true,
+					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+					max_file_lines = nil, -- Do not enable for files with more than n lines, int
+					-- colors = {}, -- table of hex strings
+					-- termcolors = {} -- table of colour name strings
+				},
+			})
+		end,
+	},
 }
