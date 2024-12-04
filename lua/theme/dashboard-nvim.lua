e24621c4da49e84e1a803b82fe7c70961937bddb
Prtysil <1377374583@qq.com>
2024-12-05 01:28:11 +0800

    Add new plugins

---
+++ lua/theme/dashboard-nvim.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"nvimdev/dashboard-nvim",
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+				theme = "hyper",
+			})
+		end,
+	},
+}
