c6fa61d90df311c35965703d60fccae0582c702a
Prtysil <1377374583@qq.com>
2025-01-16 10:01:15 +0800

    Add project plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ local builtin = require("telescope.builtin")
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VeryLazy",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"ahmedkhalf/project.nvim",
+	},
 	keys = {
 		{ "<leader>p", group = "file", desc = "Telescope" },
 		{ mode = "n", "<leader>ff", builtin.find_files, desc = "Telescope find files" },
@@ -10,5 +13,6 @@ return {
 		{ mode = "n", "<leader>fg", builtin.live_grep, desc = "Telescope live grep string" },
 		{ mode = "n", "<leader>fw", builtin.grep_string, desc = "Telescope grep cursor choosed string" },
 		{ mode = "n", "<leader>fb", builtin.buffers, desc = "Telescope buffers" },
+		{ mode = "n", "<leader>fp", "<cmd>Telescope projects<cr>", desc = "Telescope project" },
 	},
 }
