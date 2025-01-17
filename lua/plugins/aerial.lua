b3adc9ecbb7d9359ffb1fbd428163c26532a1484
Prtysil <1377374583@qq.com>
2025-01-17 10:11:09 +0800

    [Finish]aerial keymap binding

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -7,4 +7,10 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
 	},
+	keys = {
+		{ mode = "n", "}", "<cmd>AerialNext<CR>", desc = "Aerial next" },
+		{ mode = "n", "{", "<cmd>AerialPrev<CR>", desc = "Aerial Prev" },
+		{ mode = "n", "<leader>a", "<cmd>AerialToggle<CR>", desc = "Aerial Toggle" },
+		{ mode = "n", "<leader>A", "<cmd>AerialNavToggle<CR>", desc = "Aerial Nav Toggle" },
+	},
 }
